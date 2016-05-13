0131c4fe20913723bef1d13720efe2cfa98fe1e9
Gary Ellingson <gellings13@gmail.com>
2016-05-13 08:40:07 -0600

    added gps covariance and two FW ploting nodes for debuging removed the course node because it was dumb

--- src/libnmea_navsat_driver/driver.py
+++ src/libnmea_navsat_driver/driver.py
@@ -109,8 +109,8 @@ class RosNMEADriver(object):
                 longitude = -longitude
             current_fix.longitude = longitude
 
-            # hdop = data['hdop']
-            # current_fix.position_covariance[0] = hdop ** 2
+            hdop = data['hdop']
+            current_fix.covariance = hdop ** 2
             # current_fix.position_covariance[4] = hdop ** 2
             # current_fix.position_covariance[8] = (2 * hdop) ** 2  # FIXME
             # current_fix.position_covariance_type = \
