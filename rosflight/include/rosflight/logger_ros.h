37c03d5b3ca199c2422bb205790e306e5be36976
Jacob Willis <jbwillis272@gmail.com>
2020-05-21 17:02:34 -0600

    copied in files from async_comm and renamed for mavrosflight

---
+++ rosflight/include/rosflight/logger_ros.h
@@ -0,0 +1,67 @@
+/*
+ * Software License Agreement (BSD-3 License)
+ *
+ * Copyright (c) 2018 Daniel Koch.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file logger_ros.h
+ * @author Daniel Koch <danielpkoch@gmail.com>
+ */
+
+#ifndef ROSFLIGHT_LOGGER_ROS_H
+#define ROSFLIGHT_LOGGER_ROS_H
+
+#include <rosflight/mavrosflight/logger_interface.h>
+
+#include <ros/ros.h>
+
+namespace rosflight
+{
+
+/**
+ * @class LoggerROS
+ * @brief Logger implementation for ROS environments
+ *
+ * This is a convenience logger implementation for ROS-based projects.
+ * The implementation simply forwards messages to the appropriate rosconsole loggers.
+ */
+class LoggerROS : public LoggerInterface
+{
+public:
+  inline void debug(const std::string &message) override { ROS_DEBUG("[mavrosflight]: %s", message.c_str()); }
+  inline void  info(const std::string &message) override { ROS_INFO("[mavrosflight]: %s", message.c_str()); }
+  inline void  warn(const std::string &message) override { ROS_WARN("[mavrosflight]: %s", message.c_str()); }
+  inline void error(const std::string &message) override { ROS_ERROR("[mavrosflight]: %s", message.c_str()); }
+  inline void fatal(const std::string &message) override { ROS_FATAL("[mavrosflight]: %s", message.c_str()); }
+};
+
+} // namespace rosflight
+
+#endif // ROSFLIGHT_LOGGER_ROS_H
---
+++ rosflight/include/rosflight/mavrosflight/logger_interface.h
@@ -0,0 +1,81 @@
+/*
+ * Software License Agreement (BSD-3 License)
+ *
+ * Copyright (c) 2018 Daniel Koch.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file logger_interface.h
+ * @author Daniel Koch <danielpkoch@gmail.com>
+ */
+
+#ifndef MAVROSFLIGHT_LOGGER_INTERFACE_H
+#define MAVROSFLIGHT_LOGGER_INTERFACE_H
+
+#include <iostream>
+#include <string>
+
+namespace mavrosflight
+{
+
+/**
+ * \class LoggerInterface
+ * \brief Abstract base class for message handler
+ *
+ * The implementations of this class define how messages are displayed, logged,
+ * etc. To create custom behavior, derive from this base class and override the
+ * pure virtual functions.
+ */
+class LoggerInterface
+{
+public:
+  virtual void debug(const std::string& message) = 0;
+  virtual void  info(const std::string& message) = 0;
+  virtual void  warn(const std::string& message) = 0;
+  virtual void error(const std::string& message) = 0;
+  virtual void fatal(const std::string& message) = 0;
+};
+
+/**
+ * \class DefaultLogger
+ * \brief Default logger that outputs to stdout and stderr
+ */
+class DefaultLogger : public LoggerInterface
+{
+public:
+  inline void debug(const std::string &message) override { std::cout << "[mavrosflight][DEBUG]: " << message << std::endl; }
+  inline void  info(const std::string &message) override { std::cout << "[mavrosflight][INFO]: " << message << std::endl; }
+  inline void  warn(const std::string &message) override { std::cerr << "[mavrosflight][WARN]: " << message << std::endl; }
+  inline void error(const std::string &message) override { std::cerr << "[mavrosflight][ERROR]: " << message << std::endl; }
+  inline void fatal(const std::string &message) override { std::cerr << "[mavrosflight][FATAL]: " << message << std::endl; }
+};
+
+} // namespace mavrosflight
+
+#endif // MAVROSFLIGHT_LOGGER_INTERFACE_H
