dc9d0c793aeecb9a14a0392f6fa5d0f26d17a6f1
Brandon Sutherland <brandonsutherland2@gmail.com>
2023-06-28 13:39:23 -0600

    Got visualization tools working

---
+++ rosflight_utils/launch/viz_att.launch.py
@@ -0,0 +1,37 @@
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import GroupAction
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node, PushRosNamespace
+
+
+def generate_launch_description():
+    """ Launches rviz with ROSflight attitude visualization """
+
+    # Get rviz config filepath
+    rviz2_config_file = PathJoinSubstitution(
+        [get_package_share_directory('rosflight_utils'), 'rviz', 'viz_att.rviz']
+    )
+
+    # Launch rviz
+    rviz = GroupAction([
+        PushRosNamespace('viz'),
+
+        Node(package='rviz2',
+             executable='rviz2',
+             name='rviz2',
+             arguments=['-d', rviz2_config_file],
+             output='screen'),
+    ])
+
+    # Launch viz node
+    viz_node = Node(
+        package='rosflight_utils',
+        executable='viz'
+    )
+
+    return LaunchDescription([
+        rviz,
+        viz_node
+    ])
