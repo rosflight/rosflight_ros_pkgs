9de5955b68567c7c677cbfc38f6b9cd287d2e5f5
Brandon Sutherland <brandonsutherland2@gmail.com>
2023-07-19 16:47:59 -0600

    Removed gazebo macros for EOL versions

--- rosflight_sim/include/rosflight_sim/gz_compat.hpp
+++ rosflight_sim/include/rosflight_sim/gz_compat.hpp
@@ -29,12 +29,10 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef ROSFLIGHT_SIM_GZ_COMPAT_H // Include guard
+#ifndef ROSFLIGHT_SIM_GZ_COMPAT_H
 #define ROSFLIGHT_SIM_GZ_COMPAT_H
 
 #include <gazebo/gazebo.hh>
-
-#if GAZEBO_MAJOR_VERSION >= 8
 
 #include "gazebo/common/CommonTypes.hh"
 
@@ -67,38 +65,4 @@ using GazeboQuaternion = ignition::math::Quaterniond;
 #define GZ_COMPAT_GET_GRAVITY(WORLD_PTR) (WORLD_PTR)->Gravity()
 #define GZ_COMPAT_GET_MASS(INERTIAL_PTR) (INERTIAL_PTR)->Mass()
 
-#else // I.E. GAZEBO_MAJOR_VERSION < 8
-
-using GazeboVector = gazebo::math::Vector3;
-using GazeboPose = gazebo::math::Pose;
-using GazeboQuaternion = gazebo::math::Quaternion;
-
-#define GZ_COMPAT_GET_X(VECTOR) (VECTOR).x
-#define GZ_COMPAT_GET_Y(VECTOR) (VECTOR).y
-#define GZ_COMPAT_GET_Z(VECTOR) (VECTOR).z
-#define GZ_COMPAT_GET_W(VECTOR) (VECTOR).w // For quaternions
-#define GZ_COMPAT_SET_X(VECTOR, VALUE) (VECTOR).x = (VALUE)
-#define GZ_COMPAT_SET_Y(VECTOR, VALUE) (VECTOR).y = (VALUE)
-#define GZ_COMPAT_SET_Z(VECTOR, VALUE) (VECTOR).z = (VALUE)
-#define GZ_COMPAT_SET_W(VECTOR, VALUE) (VECTOR).w = (VALUE)
-#define GZ_COMPAT_GET_POS(POSE) (POSE).pos
-#define GZ_COMPAT_GET_ROT(POSE) (POSE).rot
-#define GZ_COMPAT_GET_EULER(QUAT) (QUAT).GetAsEuler()
-#define GZ_COMPAT_GET_SIM_TIME(WORLD_PTR) (WORLD_PTR)->GetSimTime()
-#define GZ_COMPAT_GET_RELATIVE_LINEAR_VEL(LINK_PTR) (LINK_PTR)->GetRelativeLinearVel()
-#define GZ_COMPAT_GET_WORLD_LINEAR_VEL(LINK_PTR) (LINK_PTR)->GetWorldLinearVel()
-#define GZ_COMPAT_GET_RELATIVE_ANGULAR_VEL(LINK_PTR) (LINK_PTR)->GetRelativeAngularVel()
-#define GZ_COMPAT_GET_WORLD_COG_POSE(LINK_PTR) (LINK_PTR)->GetWorldCoGPose()
-#define GZ_COMPAT_GET_WORLD_POSE(LINK_PTR) (LINK_PTR)->GetWorldPose()
-#define GZ_COMPAT_GET_RELATIVE_FORCE(LINK_PTR) (LINK_PTR)->GetRelativeForce()
-#define GZ_COMPAT_GET_ENTITY(WORLD_PTR, FRAME_ID_PTR) (WORLD_PTR)->GetEntity((FRAME_ID_PTR))
-#define GZ_COMPAT_GET_WORLD_LINEAR_ACCEL(LINK_PTR) (LINK_PTR)->GetWorldLinearAccel()
-#define GZ_COMPAT_GET_LENGTH(VECTOR) (VECTOR).GetLength()
-#define GZ_COMPAT_DISCONNECT_WORLD_UPDATE_BEGIN(CONNECTION) \
-  gazebo::event::Events::DisconnectWorldUpdateBegin((CONNECTION))
-#define GZ_COMPAT_GET_GRAVITY(WORLD_PTR) (WORLD_PTR)->GetPhysicsEngine()->GetGravity()
-#define GZ_COMPAT_GET_MASS(INERTIAL_PTR) (INERTIAL_PTR)->GetMass()
-
-#endif // GAZEBO_MAJOR_VERSION >= 8
-
-#endif // Include guard
+#endif // ROSFLIGHT_SIM_GZ_COMPAT_H
