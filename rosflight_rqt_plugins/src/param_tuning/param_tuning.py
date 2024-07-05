f21fb8ef81d11b5ccd36a70a2f290489dad1379f
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-07-05 13:29:11 -0600

    Added a number to the title for multiple windows

--- rosflight_rqt_plugins/src/param_tuning/param_tuning.py
+++ rosflight_rqt_plugins/src/param_tuning/param_tuning.py
@@ -12,9 +12,10 @@ class ParamTuning(Plugin):
         self._node = context.node
         self._args = self._parse_args(context.argv())
         self._widget = ParamTuningWidget()
-
+        if context.serial_number() > 1:
+            self._widget.setWindowTitle(
+                self._widget.windowTitle() + (' (%d)' % context.serial_number()))
         context.add_widget(self._widget)
 
     def _parse_args(self, argv):
         return None
-
