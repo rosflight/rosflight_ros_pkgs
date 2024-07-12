e00aa7f0ce4835a074158217c52350a420e681f5
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-07-12 14:52:13 -0600

    Made plotting prettier

--- rosflight_rqt_plugins/src/param_tuning/param_tuning_plotter.py
+++ rosflight_rqt_plugins/src/param_tuning/param_tuning_plotter.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 
-from python_qt_binding.QtWidgets import QWidget, QVBoxLayout
+from python_qt_binding.QtGui import QFont
+from python_qt_binding.QtWidgets import QWidget, QVBoxLayout, QApplication
 
 class ParamTuningPlotter(QWidget):
     def __init__(self, config: dict, paramClient, layout: QVBoxLayout):
@@ -15,11 +16,25 @@ class ParamTuningPlotter(QWidget):
         self.canvas = FigureCanvasQTAgg(Figure())
         layout.addWidget(self.canvas)
 
+        # Get current font size (not sure if this is needed on non-HiDPI screens?)
+        default_font: QFont = QApplication.font()
+        if default_font.pointSize() > 0:
+            self.fontSize = default_font.pointSize() * 2
+        elif default_font.pixelSize() > 0:
+            self.fontSize = default_font.pixelSize() * 2
+
+        self.plot()
+
+    def plot(self):
         self.ax = self.canvas.figure.subplots()
-        self.ax.set_xlabel('Time (s)')
-        self.ax.set_ylabel('Value')
+        self.ax.set_xlabel('Time (s)', fontsize=self.fontSize)
+        self.ax.set_ylabel('Value', fontsize=self.fontSize)
+        self.ax.tick_params(axis='both', labelsize=self.fontSize)
         self.ax.grid(True)
         x = np.linspace(0, 10, 100)
-        y = np.sin(x)
-        self.ax.plot(x, y)
+        y1 = np.sin(x)
+        y2 = np.cos(x)
+        self.ax.plot(x, y1, label='sin(x)')
+        self.ax.plot(x, y2, label='cos(x)')
+        self.ax.legend(loc='upper right', fontsize=self.fontSize)
         self.canvas.draw()
