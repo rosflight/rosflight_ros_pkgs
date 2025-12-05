16c7d380de96f7948570605b88b133255084dcdb
Jacob Moore <jacobmoor2@gmail.com>
2025-12-05 13:16:15 -0700

    refactor standalone viz transcriber for cleaner code

--- rosflight_sim/simulators/standalone_sim/include/standalone_viz_transcriber.hpp
+++ rosflight_sim/simulators/standalone_sim/include/standalone_viz_transcriber.hpp
@@ -31,13 +31,12 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef ROSFLIGHT_SIM_STANDALONE_SIM_HPP
-#define ROSFLIGHT_SIM_STANDALONE_SIM_HPP
+#ifndef STANDALONE_VIZ_TRANSCRIBER_HPP
+#define STANDALONE_VIZ_TRANSCRIBER_HPP
 
-#include <ament_index_cpp/get_package_share_directory.hpp>
-#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <vector>
+
 #include <rclcpp/rclcpp.hpp>
-#include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <visualization_msgs/msg/marker.hpp>
 
@@ -46,27 +45,24 @@
 namespace rosflight_sim
 {
 
-class RvizPublisher : public rclcpp::Node
+class StandaloneVizTranscriber : public rclcpp::Node
 {
 public:
-  RvizPublisher();
+  StandaloneVizTranscriber();
 
 private:
-  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr rviz_wp_pub_;
   rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr rviz_mesh_pub_;
   rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr rviz_aircraft_path_pub_;
   rclcpp::Subscription<rosflight_msgs::msg::SimState>::SharedPtr vehicle_state_sub_;
 
   std::unique_ptr<tf2_ros::TransformBroadcaster> aircraft_tf2_broadcaster_;
 
-  // Set up parameter handling
   OnSetParametersCallbackHandle::SharedPtr parameter_callback_handle_;
-  rcl_interfaces::msg::SetParametersResult
-  parameters_callback(const std::vector<rclcpp::Parameter> & parameters);
+  rcl_interfaces::msg::SetParametersResult parameters_callback(const std::vector<rclcpp::Parameter> & parameters);
   void declare_parameters();
 
+  void initialize_aircraft_marker();
   void state_update_callback(const rosflight_msgs::msg::SimState & state);
-  void update_list();
   void update_mesh();
   void update_aircraft_history();
 
@@ -75,7 +71,6 @@ private:
   // Persistent rviz markers
   visualization_msgs::msg::Marker aircraft_;
   visualization_msgs::msg::Marker aircraft_history_;
-  std::vector<geometry_msgs::msg::Point> aircraft_history_points_;
 
   int i_;
 };
