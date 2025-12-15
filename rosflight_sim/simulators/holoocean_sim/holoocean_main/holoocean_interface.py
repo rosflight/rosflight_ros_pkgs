ce9f1054c2a2bdde3322f66986e8bbe7c4994c24
andema04 <andema.mongane@gmail.com>
2025-12-15 11:53:47 -0700

    feat: holoocean simulation files

---
+++ rosflight_sim/simulators/holoocean_sim/holoocean_main/holoocean_interface.py
@@ -0,0 +1,120 @@
+from abc import ABC, abstractmethod
+# from holoocean_msgs.msg import RangeFinderSensor, RGBCameraSensor
+import numpy as np
+from pathlib import Path
+import holoocean
+import json
+
+
+class HolooceanInterface():
+    """
+    Lightweight wrapper around holoocean for loading a scenario file and advancing the simulation,
+    returning sensor data.
+    """
+    def __init__(self, scenario_path, tps=30, show_viewport=True, render_quality=None):
+        """
+        Parameters:
+            scenario_path (str or Path): Path to the scenario file
+            tps (int): ticks per second for the simulation
+            show_viewport (bool): Whether to show the simulation viewport
+            render_quality (int or None): Render quality setting for the simulation
+        
+        Attributes:
+            env: The holoocean environment instance
+            agent: The main agent in the scenario
+            sensors: The sensors attached to the main agent
+        """
+        self.tps = tps
+        scenario_path = Path(scenario_path)     # Ensure a Path object for the scenario path.
+
+        # Load scenario file.
+        with open(str(scenario_path), 'r') as f:
+            scenario = json.load(f)
+
+        # Define necesarry sensors for detecting collisions.
+        collision_sensors = [
+            {
+            "sensor_name": "GroundRange",
+            "sensor_type": "RangeFinderSensor",
+            "socket": "COM",
+            "configuration": {
+                "LaserMaxDistance": 100.0,
+                "LaserCount": 1,
+                "LaserAngle": -90.0,
+                "LaserDebug": False
+            }
+            },
+            {
+            "sensor_name": "HorizontalRange",
+            "sensor_type": "RangeFinderSensor",
+            "socket": "COM",
+            "configuration": {
+                "LaserMaxDistance": 50.0,
+                "LaserCount": 6,
+                "LaserAngle": 0.0,
+                "LaserDebug": False
+            }
+            }
+        ]
+
+        # Append sensors to main agent's sensor list.
+        scenario["agents"][0]["sensors"].extend(collision_sensors)
+
+        # Create environment using ticks_per_sec parameter.
+        self.env = holoocean.make(scenario_cfg=scenario, show_viewport=show_viewport, ticks_per_sec=self.tps)
+        self.agent = self.env.agents[scenario["main_agent"]]
+        self.sensors = self.agent.sensors
+        self.ros_publish = scenario["ros_publish"]
+
+        # Initial step to populate state.
+        command = np.zeros(6) if 'fixedwing' in str(scenario_path) else np.zeros(4)
+        self.state = self.env.step(command)
+        self.render_quality = render_quality
+
+    def set_render_quality(self, value=None):
+        """
+        Set the render quality of the environment.
+        value (int or None): Render quality setting. If None, uses the existing setting.
+        """
+        if value is not None:
+            self.render_quality = value
+        if self.render_quality is not None:
+            self.env.set_render_quality(self.render_quality)
+
+    def set_agent_state(self, location, rotation, velocity, angular_velocity):
+        """
+        Update an agent's physics state in the simulation (does not advance sim).
+        Parameters:
+            location (ndarray): New position of the agent
+            rotation (ndarray): New orientation of the agent (e.g., Euler angles or quaternion)
+            velocity (ndarray): New linear velocity of the agent
+            angular_velocity (ndarray): New angular velocity of the agent
+        """
+        self.agent.set_physics_state(location, rotation, velocity, angular_velocity)
+    
+    def tick(self):
+        """
+        Progresses the simulation by one tick and update sensor data.
+        Returns:
+            sensors_data (dict): dictionary mapping agents to latest sensor data
+        """
+        sensors_data = self.env.tick()
+        return sensors_data
+    
+    def sensor_callback(self, sensor_name):
+        """
+        Get latest sensor data from the simulation
+        Parameters:
+            sensor_name (str): Name of the sensor to retrieve data from
+        Returns:
+            sensor_data: The latest data from the specified sensor
+        """
+        sensor_data = self.sensors[sensor_name].sensor_data
+        return sensor_data
+
+    def reset_environment(self):
+        """
+        Reset the environment and repopulate state
+        """
+        self.state = self.env.reset()
+        self.set_render_quality()
---
+++ rosflight_sim/simulators/holoocean_sim/launch/fixedwing_holoocean.launch.py
@@ -0,0 +1,100 @@
+"""
+File: fixedwing_standalone_io_joy.launch.py
+Author: Brandon Sutherland, Jacob Moore
+Created: February 3, 2025
+Last Modified: March 25, 2025
+Description: ROS2 launch file used to launch all the nodes for a fixedwing standalone simulator
+"""
+
+import os
+import sys
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    """This is a launch file that runs the bare minimum requirements fly a fixedwing in a standalone simulator"""
+    dynamics_param_file = os.path.join(get_package_share_directory('rosflight_sim'), 'params', 'anaconda_dynamics.yaml')
+
+    # Declare launch arguments
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Whether the nodes will use sim time or not"
+    )
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    ##########
+    # Launch #
+    ##########
+
+    env_arg = DeclareLaunchArgument(
+        'env',
+        default_value='default',
+        description='Name of the environment to load in HoloOcean'
+    )
+
+    # Start simulator
+    simulator_launch_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory("rosflight_sim"),
+                "launch/holoocean_sim.launch.py",
+            )
+        ]),
+        launch_arguments={
+            'agent': 'fixedwing',
+            'env': LaunchConfiguration('env'),
+        }.items()
+    )
+
+
+    # Start common nodes
+    common_nodes_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("rosflight_sim"),
+                "launch", "common_nodes_standalone.launch.py"
+            )
+        ),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+            'dynamics_param_file': dynamics_param_file,
+        }.items()
+    )
+
+    # Start forces and moments
+    fw_forces_moments_node = Node(
+        package="rosflight_sim",
+        executable="fixedwing_forces_and_moments",
+        name='fixedwing_forces_and_moments',
+        output="screen",
+        parameters=[
+            {"use_sim_time": use_sim_time}, dynamics_param_file,
+        ],
+    )
+
+    # Start dynamics node
+    standalone_dynamics_node = Node(
+        package="rosflight_sim",
+        executable="standalone_dynamics",
+        name='standalone_dynamics',
+        output="screen",
+        parameters=[{"use_sim_time": use_sim_time}, dynamics_param_file]
+    )
+
+    return LaunchDescription(
+        [   
+            env_arg,
+            use_sim_time_arg,
+            simulator_launch_include,
+            common_nodes_include,
+            fw_forces_moments_node,
+            standalone_dynamics_node,
+        ]
+    )
